a0d21996f5867c4f2043d13f7f22b325bb692a62
Connor Dougherty-Keehan <connormdk@gmail.com>
2025-05-18 09:45:18 +1000

    Add keep alive controller

---
+++ WarmReminders.Api/Controllers/KeepAliveController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarmReminders.Api.Controllers;
+
+[AllowAnonymous]
+public class KeepAliveController : LocalReminderBaseController
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
